757bd4ab600666d0f26618a41e1f8567468ea581
darrius <mohamedtine17@gmail.com>
2023-11-12 15:32:23 +0000

    footer presque finit

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,16 @@
 import { signIn } from "next-auth/react";
 import { SessionProvider } from "next-auth/react";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 import Style from "@/styles/page.module.css"
 
 export default function Home() {
   return <main  className={Style.main}>
       <Header/>
-      <section>
+      <section style={{marginBottom:70}}>
         En Cours de developpement
         <button onClick={() => signIn()}>Sign in</button>
       </section>
+      <Footer/>
  </main>;
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,198 @@
+import Link from "next/link";
+import Image from "next/image";
+import Email from "@/public/footer/email.svg"
+import Location from "@/public/footer/location.svg"
+import Phone from "@/public/footer/phone.svg"
+import End from "@/public/footer/flemme.svg"
+import Line from "@/public/footer/lineChelou.svg"
+import Facebook from "@/public/footer/facebook.svg"
+import Insta from "@/public/footer/insta.svg"
+import Youtube from "@/public/footer/youtub.svg"
+import Twitter from "@/public/footer/twitter.svg"
+
 export default function Footer(){
-    return<>
-        <h1>Test</h1>
-    </>
+    return <footer>
+        <div style={{
+            display:"flex",
+            justifyContent:"space-evenly",
+            alignItems:"start",
+        }}>
+        <div>
+            <p style={{
+                marginBottom:10,
+                fontWeight:500,
+            }}>Reseaux</p>
+            <div style={{
+                display:"flex",
+                alignItems:"center",
+            }}>
+                <Image src={Facebook}
+                       alt={"facebook"}
+                        width={40}
+                       height={40}
+                       style={{
+                           marginRight:5
+                       }}
+                />
+                <Image src={Twitter}
+                       alt={"twitter"}
+                       width={40}
+                       height={40}
+                       style={{
+                           marginRight:5
+                       }}
+                />
+                <Image src={Insta}
+                       alt={"Instagram"}
+                       width={40}
+                       height={40}
+                       style={{
+                           marginRight:5
+                       }}
+                />
+                <Image src={Youtube}
+                       alt={"Youtube"}
+                       width={40}
+                       height={40}
+                />
+            </div>
+        </div>
+
+        <div style={{
+            display:"flex",
+            flexDirection:"column",
+        }}>
+            <p style={{
+                marginBottom:10,
+                fontWeight:500,
+            }}>
+                Liens Rapides
+            </p>
+            <Image src={Line}
+                   alt="Line"
+                   style={{
+                       marginBottom:10,
+                   }}/>
+            <Link href={""} style={{
+                marginBottom:15
+            }}>
+                <span style={{
+                    color:"#F875AA",
+                    fontWeight:600,
+                    marginRight:10,
+                }}> {'>'} </span> Home
+            </Link>
+            <Link href={""} style={{
+                marginBottom:15
+            }}>
+                <span style={{
+                    color: "#F875AA",
+                    fontWeight:600,
+                    marginRight:10,
+                }}> {'>'} </span>
+                A propos de nous
+            </Link>
+            <Link href={""} >
+                <span style={{
+                    color: "#F875AA",
+                    fontWeight:600,
+                    marginRight:10,
+                }}> {'>'} </span>
+                Communaute
+            </Link>
+        </div>
+
+            <div style={{
+                display:"flex",
+                flexDirection:"column",
+            }}>
+                <p style={{
+                    marginBottom:10,
+                    fontWeight:500,
+                }}>Blog</p>
+                <Image src={Line}
+                       alt="Line"
+                       style={{
+                           marginBottom:10,
+                       }}/>
+                <Link href={""} style={{
+                    marginBottom: 15
+                }}>
+                <span style={{
+                    color: "#F875AA",
+                    fontWeight:600,
+                    marginRight:10,
+                }}> {'>'} </span>
+                    Home
+                </Link>
+                <Link href={""} style={{
+                    marginBottom: 15
+                }}>
+                <span style={{
+                    color: "#F875AA",
+                    fontWeight:600,
+                    marginRight:10,
+                }}> {'>'} </span>
+                    A propos de nous
+                </Link>
+            </div>
+
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+        }}>
+            <p style={{
+                marginBottom:10,
+                fontWeight:500,
+            }}>Contact</p>
+                <Image src={Line}
+                       alt="Line"
+                       style={{
+                    marginBottom:10,
+                    }}
+                />
+            <Link href={""} style={{
+                marginBottom:15,
+                display:"flex",
+                alignItems:"center",
+            }}>
+                <Image
+                    src={Location}
+                    alt={"Lieu"}
+                    width={30}
+                    height={30}/>
+                <p>Dakar Plateau, Rue Saint Michel</p>
+            </Link>
+            <Link href={""} style={{
+                marginBottom:15,
+                display:"flex",
+                alignItems:"center",
+            }}>
+                <Image
+                    src={Email}
+                    alt={"Email"}
+                    width={30}
+                    height={30}/>
+                <p>Ucao@Ucao.Edu.sn</p>
+            </Link>
+            <Link href={""} style={{
+                marginBottom:15,
+                display:"flex",
+                alignItems:"center",
+            }}>
+                <Image
+                    src={Phone}
+                    alt={"Phone"}
+                    width={30}
+                    height={30}/>
+                <p>+221 800 00 50 50</p>
+            </Link>
+        </div>
+        </div>
+        <div>
+            <Image src={End}
+                   alt={"Fin"}
+            />
+        </div>
+    </footer>
 }
