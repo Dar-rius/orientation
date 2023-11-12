212f62034be0f9e1eebbe1db48a2eaf12848af31
darrius <mohamedtine17@gmail.com>
2023-11-12 13:12:52 +0000

    finish style header

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { signIn } from "next-auth/react";
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/header";
+import Style from "@/styles/page.module.css"
 
 export default function Home() {
-  return (
-      <main>
+  return <main  className={Style.main}>
+      <Header/>
+      <section>
         En Cours de developpement
         <button onClick={() => signIn()}>Sign in</button>
-      </main>
-  );
+      </section>
+ </main>;
 }
